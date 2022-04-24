81400fe8cf0245ebffdc1367c8684936af92ef43
s10223054f <103989827+s10223054f@users.noreply.github.com>
2022-04-24 14:03:58 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -15,7 +15,7 @@ public class Question2
     //System.out.print("Enter your weight (kg): ");
     double weight = in.nextDouble();
     double bmi = weight/(height*height);
-    //System.out.println("Your BMI is " + bmi);
+    System.out.println(bmi);
     
   }
 }
