2cd4ac1426ca05638689c261eb06c9e407dbc376
s10223054f <103989827+s10223054f@users.noreply.github.com>
2022-04-24 16:07:47 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -30,13 +30,31 @@ public class Question5
     System.out.print("Enter a number: ");
     int no = in.nextInt();
     ArrayList<Integer> dataList = new ArrayList<Integer>();
-    int x = 0;
-    while (x <= no)
+    ArrayList<Integer> counter = new ArrayList<Integer>();
+    int count = 1;
+    for (int x = 0; x < no; x++)
     {
       System.out.print("Add a number: ");
       int y = in.nextInt();
-      dataList.add(y);
-      x++;
+      if (!dataList.contains(y))
+      {
+        dataList.add(y);
+        counter.add(count);
+      }
+      else
+      {
+        int position = dataList.indexOf(y);
+        counter.set(position,counter.get(position)+1);
+      }
     }
+    int mode = 0;
+    for (int o : counter)
+    {
+      if (o > mode)
+      {
+        mode = o;
+      }
+    }
+    System.out.print("Mode:" + dataList.get(counter.indexOf(mode)));
   }
 }
